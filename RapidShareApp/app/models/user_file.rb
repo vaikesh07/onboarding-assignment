3e8b2a4ccaccf21f1c49c720648da31810f40e0e
vaikesh07 <vaibhav.k@browserstack.com>
2025-07-08 15:42:30 +0530

    added length validation line

--- RapidShareApp/app/models/user_file.rb
+++ RapidShareApp/app/models/user_file.rb
@@ -2,6 +2,9 @@ class UserFile < ApplicationRecord
   belongs_to :user
   mount_uploader :file, FileUploader # This is the key line for CarrierWave
 
+  # Add this validation
+  validates :name, length: { maximum: 255 }, allow_blank: true
+
   # Add the same sharing logic as before
   before_update :generate_share_token, if: -> { shareable? && share_token.blank? }
   before_update :clear_share_token, unless: :shareable?
